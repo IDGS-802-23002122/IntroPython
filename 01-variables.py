3cf0247eb2ff405d62564b828608260d7da66736
Guzak821 <pichisguzma01@gmail.com>
2026-01-14 20:07:06 -0600

    primer commit

---
+++ 01-variables.py
@@ -0,0 +1,14 @@
+print("Hello, World!")
+
+num1 = 10
+num2 = 20
+sum_result = num1 + num2
+print("The sum of", num1, "and", num2, "is", sum_result)
+
+num3=input("Enter a number: ")
+print("You entered:", num3)
+
+num4=input("Enter a number: ")
+print("You entered:", num4)
+suma2=int(num3)+int(num4)
+print("la suma de {} + {} es: {}".format(num3,num4,suma2))
